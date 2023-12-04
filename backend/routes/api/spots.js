c352937d06632cb278a69aa909f44833c3e7a6cb
Hillary Jackson <hill_a_ry@hotmail.com>
2023-12-04 00:21:20 -0500

    fixed 404 to 500 status code

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -191,7 +191,7 @@ router.post('/:spotId/reviews', requireAuth, validateReview, async (req, res)=>{
         }
     }) 
     if(reviews.length >= 1 ){
-        return res.status(403).json({
+        return res.status(500).json({
             message: "User already has a review for this spot"
         });
     }
