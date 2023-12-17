5fff6b2a75b7955e0e693d08ff354824410c3e82
Hillary Jackson <hill_a_ry@hotmail.com>
2023-12-17 11:28:14 -0500

    preparing for deployment

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,39 +1,24 @@
+
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import OpenModalButton from '../OpenModalButton/OpenModalButton';
-import LoginFormModal from '../LoginFormModal/LoginFormModal';
 import './Navigation.css';
 
 function Navigation({ isLoaded }) {
-    const sessionUser = useSelector((state) => state.session.user);
-  
-    const sessionLinks = sessionUser ? (
+  const sessionUser = useSelector(state => state.session.user);
+
+  return (
+    <ul>
       <li>
-        <ProfileButton user={sessionUser} />
+        <NavLink exact to="/">Home</NavLink>
       </li>
-    ) : (
-      <>
+      {isLoaded && (
         <li>
-          <OpenModalButton
-            buttonText="Log In"
-            modalComponent={<LoginFormModal />}
-          />
+          <ProfileButton user={sessionUser} />
         </li>
-        <li>
-          <NavLink to="/signup">Sign Up</NavLink>
-        </li>
-      </>
-    );
-  
-    return (
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        {isLoaded && sessionLinks}
-      </ul>
-    );
-  }
-  
-  export default Navigation;
+      )}
+    </ul>
+  );
+}
+
+export default Navigation;
