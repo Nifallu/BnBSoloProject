0c5637030d8a03d214433d075d04d5c8d39961df
Hillary Jackson <hill_a_ry@hotmail.com>
2023-12-17 11:49:38 -0500

    minor changes to backend index.js had some extra repeated lines

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,17 +1,3 @@
-// backend/routes/index.js
-const express = require('express');
-const router = express.Router();
-
-const apiRouter = require('./api');
-
-router.use('/api', apiRouter);
-
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
-
-
 // Add a XSRF-TOKEN cookie in development
 const express = require('express');
 const router = express.Router();
